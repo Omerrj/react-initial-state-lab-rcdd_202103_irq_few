ee610cd23125eba02d870a1c676e64582db3ad96
Omer Sabah <omerrj99@gmail.com>
2021-06-07 22:08:31 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 
 export default function ({initialCount}) {
 
-const [secondsLeft,setSecondLeft]=useState(initialCount)
+const [secondsLeft,setSecondLeft]=useState(initialCount);
 
  
     const message = secondsLeft === 0 ? 'Boom!' : `${secondsLeft} seconds left before I go boom!`;

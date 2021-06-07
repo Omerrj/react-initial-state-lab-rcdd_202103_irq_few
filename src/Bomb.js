b3556e06d3127872b20cd30a8f736e9ad298750b
Omer Sabah <omerrj99@gmail.com>
2021-06-07 22:06:16 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,14 @@
-// your Bomb code here!
+import React,{useState} from 'react';
+
+export default function ({initialCount}) {
+
+const [secondsLeft,setSecondLeft]=useState(initialCount)
+
+ 
+    const message = secondsLeft === 0 ? 'Boom!' : `${secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+  }
+
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,9 @@
-// your ImageSlider code here!
+import React,{useState} from 'react';
+
+export default function () {
+
+const [currentSlideIndex,setCurrentSlideIndex]=useState(0);
+
+  return <h1>I am on slide {currentSlideIndex}</h1>;
+    
+}
